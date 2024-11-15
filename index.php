00430cd661749cb8f8fbe55dc8d9b2b891ac2d9b
valziyan <valziyan@gmail.com>
2024-11-15 19:30:40 +0800

    firt commit

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+// define server variables
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "im101_c";
+
+$name = $_POST['name'];
+
+try {
+
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    if ($name) {
+        $sql = "INSERT INTO user (name) VALUES ('$name')";
+        $conn->exec($sql);
+        echo "new registration!";
+    }
+
+
+    // 
+
+} catch(PDOException $e) {
+
+    echo "Connection failed: " . $e->getMessage();
+
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>User Registration!</h1>
+
+    <form action="" method="post">
+        Name: <input type="text" name="name"><br>
+        <input type="submit" value="Register">
+    </form>
+
+</body>
+</html>
